0a09d87423a2bb39f39642fd4c263db4a3b18856
DorotikPMA <l_dorotik@utb.cz>
2020-01-30 15:50:51 +0100

    API request funkcni

--- app/src/main/java/cz/education/projectpma/MainActivity.java
+++ app/src/main/java/cz/education/projectpma/MainActivity.java
@@ -1,11 +1,20 @@
 package cz.education.projectpma;
 
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,60 +27,46 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    class Rates extends AsyncTask<String,Void,String>{ // Stringg - URL, void, String - navratova hodnota
+    private TextView tw;
 
-        @Override
-        protected String doInBackground(String... address) {
-            //String.. znamena, ze muze byt poslano vice adres, tvari se jako pole
-            try {
-                URL url = new URL(address[0]);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                //nastavuje connection na adresu
-                connection.connect();
+    private void getJson(){
+        String url = "https://openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22";
 
-                //ziskani dat z url
-                InputStream is = connection.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-
-                //ziskani dat a vraceni jako string
-                int data = isr.read();
-                String content = "";
-                char ch;
-                while (data != 0) {
-                    ch = (char) data;
-                    content = content + ch;
-                    data = isr.read();
-                }
-                Log.i("Content",content);
-                return content;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+        StringRequest request = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String string) {
+                parseJsonData(string); // Pošle získané data do parseru
             }
-
-
-            return null;
-
-        }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
+        rQueue.add(request);
     }
 
+
+    public void parseJsonData(String jsonString){
+        tw.setText(jsonString);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tw = findViewById(R.id.textView);
+
+        getJson();
+
         Log.i("HEllo World","whats'up");
 
-        String content;
-        Rates rates =  new Rates();
-        try {
-            content = rates.execute("http://data.fixer.io/api/latest?access_key=360b47de32cb66af0262ed6d0c50863c&symbols=USD,AUD,CAD,PLN,MXN&format=1").get();
-            //test
-            Log.d("contentData",content);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
+
+
+
+
+
 }
